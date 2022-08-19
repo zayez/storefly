e42c5db95fce80029b757cdad999ff4ba1b6d4d7
zayez <zayezuno@protonmail.com>
2022-08-19 13:50:58 -0300

    Document enum StatusCode

--- server/types/StatusCode.js
+++ server/types/StatusCode.js
@@ -1,15 +1,32 @@
+/**
+ * Enum for response status code.
+ * @readonly
+ * @enum {integer}
+ */
 const StatusCode = {
+  /** @member {integer} */
   Ok: 200,
+  /** @member {integer} */
   Created: 201,
+  /** @member {integer} */
   BadRequest: 400,
+  /** @member {integer} */
   Forbidden: 403,
+  /** @member {integer} */
   NotFound: 404,
+  /** @member {integer} */
   Conflict: 409,
+  /** @member {integer} */
   Unprocessable: 422,
+  /** @member {integer} */
   Error: 500,
+  /** @member {integer} */
   SignUpError_UpdateUserFailed: 500,
+  /** @member {integer} */
   SignUpError_CreateUserFailed: 500,
+  /** @member {integer} */
   SignUpError_CleanupUserFailed: 500,
+  /** @member {integer} */
   SignUpError_CleanupUserSucceded: 500,
 }
 
