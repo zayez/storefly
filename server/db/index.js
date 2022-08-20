d2556d92dbcbe0ff365ba8a177eec421404612d1
zayez <zayezuno@protonmail.com>
2022-08-20 18:49:17 -0300

    Fix missing env var in test

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || 'development'
+const environment = process.env.NODE_ENV || 'test'
 const config = require('../knexfile.js')[environment]
 const knex = require('knex')(config)
 
