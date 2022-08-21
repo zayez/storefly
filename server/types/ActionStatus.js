4c8d0403c51100570125587105b9a4a12d03d30c
zayez <zayezuno@protonmail.com>
2022-08-21 19:42:25 -0300

    Update enum

--- server/types/ActionStatus.js
+++ server/types/ActionStatus.js
@@ -7,10 +7,7 @@ const ActionStatus = {
   Conflict: 'conflict', // 409
   Unprocessable: 'Unprocessable', // 422
   Error: 'error', // 500
-  SignUpError_UpdateUserFailed: 'SignUpError_UpdateUserFailed', // 500
-  SignUpError_CreateUserFailed: 'SignUpError_CreateUserFailed', // 500
-  SignUpError_CleanupUserFailed: 'SignUpError_CleanupUserFailed', // 500
-  SignUpError_CleanupUserSucceded: 'SignUpError_CleanupUserSucceded', // 500
+  CreateError: 'Create_Error', // 400 (generic error)
 }
 
 module.exports = ActionStatus
