bd547a9a48def08d54152036b835a43ff05651ca
zayez <zayezuno@protonmail.com>
2022-08-25 19:04:21 -0300

    Change date column type

--- server/db/migrations/20220825001713_orders.js
+++ server/db/migrations/20220825001713_orders.js
@@ -6,7 +6,7 @@ exports.up = async function (knex) {
   await knex.schema.createTable('orders', (table) => {
     table.increments()
     table.integer('userId').references('users.id').notNullable()
-    table.date('dateOrder')
+    table.datetime('dateOrder')
     table.timestamps(true, true, true)
   })
 
