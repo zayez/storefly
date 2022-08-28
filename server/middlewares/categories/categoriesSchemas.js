5298b0dd83ed068e13c1e733f70f0d40acd8afd3
zayez <zayezuno@protonmail.com>
2022-08-27 21:30:44 -0300

    Add validation messages (category)

--- server/middlewares/categories/categoriesSchemas.js
+++ server/middlewares/categories/categoriesSchemas.js
@@ -1,7 +1,11 @@
 const Joi = require('joi')
 
 const Create = Joi.object().keys({
-  title: Joi.string().required(),
+  title: Joi.string().required().messages({
+    'string.base': 'Invalid type, title must be a string',
+    'string.empty': 'title is not allowed to be empty',
+    'any.required': 'title is required',
+  }),
 })
 
 const Update = Joi.object().keys({
