69629ad58e20c20312365a2517cfbc742f67cc5b
zayez <zayezuno@protonmail.com>
2022-08-30 14:06:19 -0300

    Use STATUS enum

--- server/helpers/middlewareHelpers.js
+++ server/helpers/middlewareHelpers.js
@@ -1,5 +1,6 @@
 const ActionStatus = require('../types/ActionStatus')
 const SuccessStatuses = [ActionStatus.Ok, ActionStatus.Created]
+const STATUS = require('../types/StatusCode')
 
 /**
  * Sets the response based on the action with the payload.
@@ -32,64 +33,59 @@ function getContentType(action) {
 }
 
 function getResponse(action) {
-  let status
   switch (action) {
     case ActionStatus.Ok:
-      status = 200
-      break
+      return STATUS.Ok
     case ActionStatus.Created:
-      status = 201
-      break
+      return STATUS.Created
     default:
-      status = 200
-      break
+      return STATUS.Ok
   }
-  return status
 }
 
 function getResponseError(action) {
   let status, title, detail
   switch (action) {
     case ActionStatus.BadRequest:
-      status = 400
+      status = STATUS.BadRequest
       title = 'Bad Request'
       detail =
         'The server could not understand the request due to invalid syntax.'
       break
     case ActionStatus.Unauthorized:
-      status = 401
+      status = STATUS.Unauthorized
       title = 'Unauthorized'
       detail =
         'The request lacks valid authentication credentials for the requested resource.'
       break
     case ActionStatus.Forbidden:
-      status = 403
+      status = STATUS.Forbidden
       title = 'Forbidden'
       detail = 'The client does not have access rights to the content'
       break
 
     case ActionStatus.Conflict:
-      status = 409
+      status = STATUS.Conflict
       title = 'Conflict'
       detail = 'The request conflicts with the current state of the server'
       break
     case ActionStatus.Unprocessable:
-      status = 422
+      status = STATUS.Unprocessable
       title = 'Unprocessable'
       detail = 'The request was unable to process the contained entity'
       break
     case ActionStatus.Error:
-      status = 500
+      status = STATUS.Error
       title = 'Internal server error'
       detail = 'A fatal error occured'
       break
     case ActionStatus.CreateError:
-      status = 400
+      status = STATUS.BadRequest
       title = 'Create - failed to create'
       detail = 'The server was unable to insert the data.'
       break
     default:
-      status = 404
+      status = STATUS.NotFound
       title = 'Not Found'
       detail = 'The server can not find the requested resource.'
       break
