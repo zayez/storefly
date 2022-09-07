09feec6166decfd472587c08950cbfd76b12ce47
zayez <zayezuno@protonmail.com>
2022-09-07 12:28:23 -0300

    Add signIn page

--- client/pages/signin.js
+++ client/pages/signin.js
@@ -1,7 +1,59 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 const SignIn = () => {
+  const router = useRouter()
+  const baseUrl = `/api`
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const url = `${baseUrl}/signin`
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    }
+    const res = await fetch(url, requestOptions)
+    if (res.status === 200) {
+      router.push('/')
+    } else {
+      console.log('Failed to signin.')
+    }
+  }
+
   return (
-    <div className="signin">
-      <h1>Sign in</h1>
+    <div className="container">
+      <div className="login">
+        <h1>Sign in</h1>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="field">
+            <div className="field-label">
+              <label>E-mail:</label>
+            </div>
+            <div className="field-body">
+              <input
+                type="text"
+                value={email}
+                onChange={({ target }) => setEmail(target?.value)}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="field-label">
+              <label>Password:</label>
+            </div>
+            <div className="field-body">
+              <input
+                type="text"
+                value={password}
+                onChange={({ target }) => setPassword(target?.value)}
+              />
+            </div>
+          </div>
+          <button className="btn">Login</button>
+        </form>
+      </div>
     </div>
   )
 }
