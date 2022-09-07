296908d9ac048af83fc63d06ab45ff5aebf290fa
zayez <zayezuno@protonmail.com>
2022-09-07 12:27:31 -0300

    Add proxy to api endpoint

--- client/next.config.js
+++ client/next.config.js
@@ -4,4 +4,13 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:2222/:path*', // Proxy to Backend
+      },
+    ]
+  },
 }
