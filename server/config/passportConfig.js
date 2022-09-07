dcb4e17e86f0fd098fee98e1fc0dc4d633bf5081
zayez <zayezuno@protonmail.com>
2022-09-07 12:25:41 -0300

    Add cookie extractor to JWT strategy

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -2,28 +2,39 @@ const JwtStrategy = require('passport-jwt').Strategy
 const LocalStrategy = require('passport-local').Strategy
 const AnonymousStrategy = require('passport-anonymous').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+const { fromExtractors, fromHeader } = ExtractJwt
 const { SECRET } = require('../config').jwt
 const User = require('../models/user')
+
+const cookieExtractor = (req) => {
+  let token = null
+  if (req && req.header.cookie) {
+    token = req.header.cookie.replace('token=', '')
+  }
+  return token
+}
+
+const opts = {}
+opts.jwtFromRequest = fromExtractors([
+  cookieExtractor,
+  fromHeader('authorization'),
+])
+opts.secretOrKey = SECRET
+
 function passportConfig(passport) {
   // JWT passport
   passport.use(
-    new JwtStrategy(
-      {
-        jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-        secretOrKey: SECRET,
-      },
-      async (payload, done) => {
-        try {
-          const user = await User.findById(payload.sub)
-          if (!user) {
-            return done(null, false)
-          }
-          done(null, user)
-        } catch (err) {
-          done(err, false)
+    new JwtStrategy(opts, async (payload, done) => {
+      try {
+        const user = await User.findById(payload.sub)
+        if (!user) {
+          return done(null, false)
         }
-      },
-    ),
+        done(null, user)
+      } catch (err) {
+        done(err, false)
+      }
+    }),
   )
 
   // Local passport
