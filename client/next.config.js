bf01c2e7cb06406a06f61fbbf8c84e38a23c3d58
zayez <zayezuno@protonmail.com>
2022-09-08 15:03:17 -0300

    Add SVG icons support (feather icons)

--- client/next.config.js
+++ client/next.config.js
@@ -5,6 +5,14 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
 
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+
   async rewrites() {
     return [
       {
