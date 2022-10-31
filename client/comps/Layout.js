7e4d598be6ca4081e4d2220a29ac715bdc397c7d
zayez <zayezuno@protonmail.com>
2022-10-31 14:52:35 -0300

    Redirect user when access admin area if loggedout

--- client/comps/Layout.js
+++ client/comps/Layout.js
@@ -1,6 +1,10 @@
 import Header from './Header'
 import Footer from './Footer'
 import Sidebar from './Sidebar'
+import { useSelector } from 'react-redux'
+import { selectAuth } from '../store/slices/authSlice'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const StoreLayout = ({ children }) => {
   return (
@@ -12,7 +16,19 @@ const StoreLayout = ({ children }) => {
   )
 }
 
+const isManager = (user) => {
+  if (!user) return false
+  const userRoles = user.roles
+  return ['admin', 'editor'].some((role) => userRoles.includes(role))
+}
+
 const AdminLayout = ({ children }) => {
+  const router = useRouter()
+  const auth = useSelector(selectAuth)
+
+  useEffect(() => {
+    if (!auth.user) router.push('/')
+  }, [])
   return (
     <div>
       <Sidebar />
