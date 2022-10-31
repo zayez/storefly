12ada4784dda3764b44fe005fa2583ed6cabdb67
zayez <zayezuno@protonmail.com>
2022-10-31 14:50:34 -0300

    Add signUp

---
+++ client/pages/signup.js
@@ -0,0 +1,74 @@
+import { useRouter } from 'next/router'
+import { useState, useRef, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Toasty from '../comps/Toasty'
+import { selectAuth, signUp } from '../store/slices/authSlice'
+
+const SignUp = () => {
+  const router = useRouter()
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const dispatch = useDispatch()
+  const auth = useSelector(selectAuth)
+
+  useEffect(() => {
+    if (auth.success) {
+      router.push('/')
+    }
+  }, [auth.success, auth.user, auth.error])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(signUp({ firstName, lastName, email, password }))
+  }
+  return (
+    <div className="container">
+      <Toasty message={auth.error} />
+      <form onSubmit={handleSubmit} className="form">
+        <div className="field">
+          <label htmlFor="E-mail">First name</label>
+          <input
+            type="text"
+            value={firstName}
+            onChange={({ target }) => setFirstName(target?.value)}
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="E-mail">Last name</label>
+          <input
+            type="text"
+            value={lastName}
+            onChange={({ target }) => setLastName(target?.value)}
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="E-mail">E-mail</label>
+          <input
+            type="text"
+            value={email}
+            onChange={({ target }) => setEmail(target?.value)}
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="E-mail">Password</label>
+          <input
+            type="text"
+            value={password}
+            onChange={({ target }) => setPassword(target?.value)}
+          />
+        </div>
+
+        <div className="field">
+          <input type="submit" value="Create account" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
--- client/store/slices/authSlice.js
+++ client/store/slices/authSlice.js
@@ -6,6 +6,7 @@ const initialState = {
   user: null,
   error: '',
   success: false,
+  // isLoggedIn: false
 }
 
 const baseUrl = `/api`
@@ -49,6 +50,31 @@ export const signUser = createAsyncThunk(
       switch (res.status) {
         case 200:
           return res.json()
+        default:
+          return rejectWithValue(ActionStatus.Error)
+      }
+    })
+  },
+)
+
+export const signUp = createAsyncThunk(
+  'auth/signUp',
+  async ({ firstName, lastName, email, password }, { rejectWithValue }) => {
+    const url = `${baseUrl}/signup`
+    const reqOpts = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ firstName, lastName, email, password }),
+    }
+    return fetch(url, reqOpts).then(async (res) => {
+      switch (res.status) {
+        case 201:
+          return res.json()
+        case 400:
+          return res.json()
+        case 422:
+          // const stuff = await res.json()
+          return rejectWithValue(await res.json())
         default:
           return rejectWithValue(ActionStatus.Error)
       }
@@ -118,6 +144,22 @@ const authSlice = createSlice({
       state.user = null
       state.success = false
       state.error = 'Whoa. Something has gone wrong.'
+    })
+
+    builder.addCase(signUp.pending, (state) => {
+      state.loading = false
+    })
+    builder.addCase(signUp.fulfilled, (state, action) => {
+      state.loading = false
+      state.user = action.payload.user
+      state.success = true
+      state.error = ''
+    })
+    builder.addCase(signUp.rejected, (state, action) => {
+      state.loading = false
+      state.user = null
+      state.success = false
+      state.error = action.payload.detail
     })
   },
 })
