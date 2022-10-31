1ca2ce8923628435091e596729f42818f8a03053
zayez <zayezuno@protonmail.com>
2022-10-31 16:47:37 -0300

    Fix persist store

--- client/store/index.js
+++ client/store/index.js
@@ -1,6 +1,19 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+  combineReducers,
+  configureStore,
+  // getDefaultMiddleware,
+} from '@reduxjs/toolkit'
 import storage from 'redux-persist/lib/storage'
-import { persistReducer, persistStore } from 'redux-persist'
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 import users from './slices/usersSlice'
 import products from './slices/productsSlice'
 import auth from './slices/authSlice'
@@ -16,6 +29,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== 'production',
 })
 
