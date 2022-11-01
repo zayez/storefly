8755ac49c6997410727210b9d5bac1ac86668ed3
zayez <zayezuno@protonmail.com>
2022-11-01 16:08:45 -0300

    Add ToastyError

--- client/comps/Toasty.js
+++ client/comps/Toasty.js
@@ -3,4 +3,27 @@ const Toasty = ({ message }) => {
   return <></>
 }
 
+const ErrorItem = ({ error }) => {
+  return <li>{error} </li>
+}
+const ErrorsList = ({ errors }) => {
+  return (
+    <ul className="errors-list">
+      {errors.map((error, i) => (
+        <ErrorItem error={error} key={i} />
+      ))}
+    </ul>
+  )
+}
+
+export const ToastyError = ({ error, errors }) => {
+  return (
+    <div className="toasty toasty-error">
+      <div className="toasty-error-title">{error}</div>
+
+      {errors ? <ErrorsList errors={errors} /> : null}
+    </div>
+  )
+}
+
 export default Toasty
