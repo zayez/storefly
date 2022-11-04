ffde6aea70a21098622e89b066c6eb4a38751f69
zayez <zayezuno@protonmail.com>
2022-11-04 17:41:17 -0300

    Add transition animations

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,14 +5,29 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from '../store'
 import '../styles/index.sass'
+import { SwitchTransition, CSSTransition } from 'react-transition-group'
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps, router }) => {
+  const nodeRef = React.useRef(null)
   const getLayout = Component.getLayout ?? storeLayout
   return (
     <React.StrictMode>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          {getLayout(<Component {...pageProps} />)}
+          {getLayout(
+            <SwitchTransition mode="out-in">
+              <CSSTransition
+                nodeRef={nodeRef}
+                key={router.pathname}
+                classNames="page"
+                timeout={300}
+              >
+                <div ref={nodeRef}>
+                  <Component {...pageProps} />
+                </div>
+              </CSSTransition>
+            </SwitchTransition>,
+          )}
         </PersistGate>
       </Provider>
     </React.StrictMode>
